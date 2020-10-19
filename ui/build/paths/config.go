b7e6a1b7560a94d82f1b2ee1bca643eb9c71d100
TextsFromYourEx <skgaming7869@gmail.com>
2020-10-19 09:23:36 +0200

    soong: Whitelist m4
    Signed-off-by: TextsFromYourEx <skgaming7689@gmail.com>
    Change-Id: If8b8238d92829c2a440b8932262cecc0da5f2235

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -96,6 +96,7 @@ var Configuration = map[string]PathConfig{
 	"javap":   Allowed,
 	"locale":  Allowed,
 	"lsof":    Allowed,
+	"m4":      Allowed,
 	"nproc":   Allowed,
 	"openssl": Allowed,
 	"pstree":  Allowed,
